3906f865c62d946e5fd2aa2d8a61d75d789c6f8e
emersondiegoduarte <emerson.diego.duarte@gmail.com>
2022-03-30 20:24:16 -0300

    commit inicial

---
+++ src/test/java/com/project/decoder/DecoderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.decoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecoderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
